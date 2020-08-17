6e2015a4c16ed0c8257d82c35ad51a679d88d569
unknown <dyomin.dyominalexander2011@yandex.ru>
2020-08-17 19:14:59 +0300

    Some scripts are added

--- index.js
+++ index.js
@@ -16,7 +16,51 @@ function GenerateRates()
     }
 }
 
+function RemoveClassFromObjects(objs, class_name)
+{
+    for (let i = 0; i < objs.length; ++i) {
+        objs[i].classList.remove(class_name);
+    }
+}
+
+function BindSelectors(selectors)
+{
+    for (let j = 0; j < selectors.length; ++j) {
+        selectors[j].addEventListener('click', e => {
+            let parent = e.srcElement;
+            RemoveClassFromObjects(selectors, 'active');
+            parent.classList.add('active');
+        });
+    }
+}
+
+function InitImgSelectors(board_list)
+{
+    for (let i = 0; i < board_list.length; ++i)
+    {
+        let selectors = board_list[i].getElementsByClassName('img-selector')[0].children[0].children;
+        BindSelectors(selectors);
+    }
+}
+
+function InitTextSelectors(board_list)
+{
+    for (let i = 0; i < board_list.length; ++i)
+    {
+        let selectors = board_list[i].getElementsByClassName('text-selector')[0].children[0].children;
+        BindSelectors(selectors);
+    }
+}
+
+function InitBoards()
+{
+    let board_list = document.getElementsByClassName('board');
+    InitImgSelectors(board_list);
+    InitTextSelectors(board_list);
+    GenerateRates();
+}
+
 function OnLoadFunction() 
 {
-    GenerateRates();
+    InitBoards();
 }
